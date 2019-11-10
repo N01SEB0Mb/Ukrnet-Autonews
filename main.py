34fed537c2c3548c0f59f4ed8d8fdb61d11cfa68
N01SEB0Mb <noisebombch@gmail.com>
2019-11-10 23:54:57 +0200

    update

--- main.py
+++ main.py
@@ -2,27 +2,23 @@
 
 # coding=utf-8
 
-import asyncio
-from asyncio import sleep
-from aiogram import Bot
-from aiogram.utils.markdown import hbold, hlink, hide_link
-
 import json
-import parser
+import source
+import asyncio
 from time import sleep
-
-with open("config.json", "rb") as config_file:
-    config = json.load(config_file)
-
-with open("tg.json", "rb") as tg_file:
-    tg_info = json.load(tg_file)
+from aiogram import Bot
+from aiogram.utils.markdown import hbold, hlink
 
 
 async def checker():
-    p = parser.Parser(config)
+    p = source.Parser(config)
+
     while True:
         with open("last.news", "rt") as last_file:
-            last = {parser.New(json.loads(line.replace("\n", ""))) for line in last_file.readlines()}
+            try:
+                last = set(map(int, last_file.read().split(" ")))
+            except ValueError:
+                last = set()
 
         last_update = p.get_last()
         news = last_update - last
@@ -37,22 +33,34 @@ async def checker():
                     await bot.send_photo(tg_info["channel_id"], image, caption=msg, parse_mode="HTML")
                 else:
                     await bot.send_message(tg_info["channel_id"], msg, parse_mode="HTML")
-            else:
-                last_update.remove(new)
 
         with open("last.news", "wt") as last_file:
-            last_file.write("\n".join(map(str, last_update)))
+            last_file.write(" ".join(map(lambda x: str(x["Id"]), last_update)))
 
         await asyncio.sleep(config["sleep"])
 
 
 if __name__ == "__main__":
-    bot = Bot(tg_info["bot_token"])
-    aio_loop = asyncio.get_event_loop()
+    with open("config.json", "rb") as config_file:
+        config = json.load(config_file)
 
-    try:
-        aio_loop.run_until_complete(checker())
-    except KeyboardInterrupt:
-        pass
-    finally:
-        aio_loop.run_until_complete(bot.close())
+    with open("tg.json", "rb") as tg_file:
+        tg_info = json.load(tg_file)
+
+    while True:
+        try:
+            bot = Bot(tg_info["bot_token"])
+            aio_loop = asyncio.get_event_loop()
+
+            try:
+                aio_loop.run_until_complete(checker())
+            except KeyboardInterrupt:
+                pass
+            except BaseException as err:
+                print(err)
+            finally:
+                aio_loop.run_until_complete(bot.close())
+        except BaseException as err:
+            print(err)
+        finally:
+            sleep(config["retry sleep"])
--- parser.py
+++ parser.py
@@ -2,107 +2,100 @@
 
 # coding=utf-8
 
-import json
-import requests
-from requests.exceptions import BaseHTTPError
 from lxml import html
-import gzip
+from html import unescape
 
 
-class New:
-    def __init__(self, info):
-        self.__dict__ = info
-
-    def __hash__(self):
-        return self.__dict__["Id"]
-
-    def __eq__(self, other):
-        return self.__hash__() == other.__hash__()
-
-    def __str__(self):
-        return json.dumps(self.__dict__, ensure_ascii=False)
-
-    def __getitem__(self, item):
-        return self.__dict__[item]
-
-
-class Parser:
-    def __init__(self, config):
-        self.last = config["last"]
-        self.link = config["main_link"]
-        self.max_len = config["max len"]
-        self.session = requests.session()
-        self.session.headers.update(config["headers"])
-
-    def get_last(self):
-        news_request = self.session.get(self.link)
-        if news_request.status_code == 200:
-            return {New(new) for new in news_request.json()["tops"][:self.last]}
-        else:
-            return set()
-
-    def get_info(self, new):
+def clear(string):
+    try:
+        while string[0] in ("\n", " "):
+            string = string[1:]
+    except IndexError:
+        pass
+    else:
         try:
-            info_request = self.session.get(new["Url"])
-        except BaseException:
-            return None
-
-        if info_request.status_code == 200 and "utf-8" in info_request.headers["content-type"].lower():
-            info_tree = html.fromstring(info_request.text)
-
-            # title
-
-            og_title = info_tree.xpath('//meta[@property="og:title" or @name="og:title"]/@content')
-            _title = info_tree.xpath('//title/text()')
-            twitter_title = info_tree.xpath('//meta[@property="twitter:title" or @name="twitter:title"]/@content')
-
-            title = self.clear(max(og_title + twitter_title + [""], key=len))
-
-            if not title:
-                try:
-                    title = self.clear(_title[0])
-                except IndexError:
-                    return None
-
-            # description
-
-            og_description = info_tree.xpath('//meta[@property="og:description" or @name="og:description"]/@content')
-            _description = info_tree.xpath('//meta[@property="description" or @name="description"]/@content')
-            twitter_description = info_tree.xpath('//meta[@name="twitter:description" or'
-                                                  '@property="twitter:description"]/@content')
-
-            description = self.clear(max(og_description + _description + twitter_description + [""], key=len))
-
-            if description:
-                while len(title) + len(description) + 16 > self.max_len:
-                    description = description[:-1]
-                while description[-1] == " ":
-                    description = description[:-1]
-                description += "...\n\n"
-
-            # image
-
-            og_image = info_tree.xpath('//meta[@property="og:image" or @name="og:image"]/@content')
-            twitter_image = info_tree.xpath('//meta[@name="twitter:image" or @property="twitter:image"]/@content')
-
-            image_url = og_image[0] if og_image else twitter_image[0] if twitter_image else None
-
-            if not description and image_url is None:
-                return None
-
-            return title, description, image_url
-
-    @staticmethod
-    def clear(string):
-        try:
-            while string[0] in ("\n", " "):
-                string = string[1:]
+            while string[-1] in ("\n", " "):
+                string = string[:-1]
         except IndexError:
             pass
-        else:
-            try:
-                while string[-1] in ("\n", " "):
-                    string = string[:-1]
-            except IndexError:
-                pass
-        return string
+    return unescape(string)
+
+
+def parse(body, partner, max_len=1024):
+    info_tree = html.fromstring(body)
+
+    # title
+    _title = info_tree.xpath('//title/text()')
+
+    if partner == "UAMotors":
+        article_title = info_tree.xpath('//h1[@class="entry-title"]/text()')
+
+        title = max(article_title + [""], key=len)
+    elif partner == "Avtoblog":
+        article_title = info_tree.xpath('//h1[1]/text()')
+
+        title = max(article_title + [""], key=len)
+    elif partner == "ТОнеТО":
+        article_title = info_tree.xpath('//h1/span/text()')
+
+        title = max(article_title + [""], key=len)
+    else:
+        og_title = info_tree.xpath('//meta[@property="og:title" or @name="og:title"]/@content')
+        twitter_title = info_tree.xpath('//meta[@property="twitter:title" or @name="twitter:title"]/@content')
+
+        title = max(og_title + twitter_title + [""], key=len)
+
+    if not title:
+        try:
+            title = _title[0]
+        except IndexError:
+            return None
+
+    # description
+
+    if partner == "UAMotors":
+        article_description = info_tree.xpath('//div[@class="td-post-content"]/p[1]/text()')
+        span_description = info_tree.xpath('//div[@class="td-post-content"]//span/text()')[1]
+
+        description = max([span_description] + article_description + [""], key=len)
+    elif partner == "Avtoblog":
+        article_description = info_tree.xpath('//div[@id="article"]//p[@style="text-align: justify;"]/text()')[0]
+
+        description = max([article_description] + [""], key=len)
+    elif partner == "ТОнеТО":
+        article_description = info_tree.xpath('//div[@class="newsText"]//p/text()')[0]
+
+        description = max([article_description] + [""], key=len)
+    else:
+        article_description = info_tree.xpath('//div[@class="article-body"]//strong/text()')  # Hyser
+        og_description = info_tree.xpath('//meta[@property="og:description" or @name="og:description"]/@content')
+        _description = info_tree.xpath('//meta[@property="description" or @name="description"]/@content')
+        twitter_description = info_tree.xpath('//meta[@name="twitter:description" or'
+                                              '@property="twitter:description"]/@content')
+
+        description = max(article_description + og_description + _description +
+                          twitter_description + [""], key=len)
+
+    if description:
+        while len(title) + len(description) + 16 > max_len:
+            description = description[:-1]
+        while description[-1] == " ":
+            description = description[:-1]
+        description = clear(description) + "...\n\n"
+
+    # image
+
+    if partner == "ТОнеТО":
+        article_image = info_tree.xpath('//div[@id="Content"]//img/@src')
+
+        image_url = article_image[0] if article_image else None
+    else:
+        og_image = info_tree.xpath('//meta[@property="og:image" or @name="og:image"]/@content')
+        twitter_image = info_tree.xpath('//meta[@name="twitter:image" or @property="twitter:image"]/@content')
+
+        image_url = og_image[0] if og_image else twitter_image[0] if twitter_image else None
+
+    if not description and image_url is None:
+        return None
+
+    return clear(title), description, image_url
