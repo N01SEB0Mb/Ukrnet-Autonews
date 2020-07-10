590d90e428dff083c6e9d18623f3aeae08270004
N01SEB0Mb <noisebombch@gmail.com>
2020-07-10 13:01:15 +0300

    fix

--- main.py
+++ main.py
@@ -30,72 +30,65 @@ async def checker():
         with open("last.news", "wt") as last_file:
             last_file.write(" ".join(map(lambda x: str(x["Id"]), last_update)))
 
-<<<<<<< HEAD
         summary = len(news)
         count = 0
 
         for new in news:
             try:
                 info = p.get_info(new)
-            except (RequestException, BaseException) as error:
-                print(time() + str(error) + ": " + str(new))
-                if config["log file"]:
-                    with open("exception.log", "a") as logfile:
-                        logfile.write(time() + str(error) + ": " + str(new) + "\n")
-=======
+            except (RequestException, BaseException) as err:
+                print(time() + str(err) + ": " + str(new))
+                if config["logging"]:
+                    with open("exception.log", "a") as logFile:
+                        logFile.write(time() + str(error) + ": " + str(new) + "\n")
         for new in news:
             try:
                 info = p.get_info(new)
-            except BaseException as error:
+            except BaseException as err:
                 print(time() + str(err) + ": " + str(new))
-                if config["log file"]:
-                    with open("exception.log", "a") as logfile:
-                        log_file.write(time() + str(err) + ": " + str(new) + "\n")
->>>>>>> e5ff7661b3e5b888face2e490afc206a577b2047
+                if config["logging"]:
+                    with open("exception.log", "a") as logFile:
+                        logFile.write(time() + str(err) + ": " + str(new) + "\n")
             else:
                 if info:
                     title, description, image = info
                     msg = f'{hbold(title)}\n\n{description}{hlink("Подробнее", new["Url"])}'
-
-<<<<<<< HEAD
                     try:
                         if image:
-                            await bot.send_photo(tg_info["channel_id"], image, caption=msg, parse_mode="HTML")
+                            await bot.send_photo(tg_info["channelId"], image, caption=msg, parse_mode="HTML")
                         else:
-                            await bot.send_message(tg_info["channel_id"], msg, parse_mode="HTML")
+                            await bot.send_message(tg_info["channelId"], msg, parse_mode="HTML")
                     except TelegramAPIError as error:
                         print(time() + str(error) + ": " + str(new))
-                        if config["log file"]:
+                        if config["logging"]:
                             with open("exception.log", "a") as logfile:
                                 logfile.write(time() + str(error) + ": " + str(new) + "\n")
                     else:
                         count += 1
                 elif info is None:
                     print(time() + "no info: " + str(new))
-                    if config["log file"]:
+                    if config["logging"]:
                         with open("exception.log", "a") as logfile:
                             logfile.write(time() + "no info: " + str(new) + "\n")
                 else:
                     print(time() + "request error: " + str(new))
-                    if config["log file"]:
+                    if config["logging"]:
                         with open("exception.log", "a") as logfile:
                             logfile.write(time() + "request error: " + str(new) + '\n')
 
         print(time() + "checking finished, posted {0}/{1}".format(count, summary))
-        if config["log file"]:
+        if config["logging"]:
             with open("exception.log", "a") as logfile:
                 logfile.write(time() + "checking finished, posted {0}/{1}\n".format(count, summary))
-=======
-                    if image:
-                        await bot.send_photo(tg_info["channel_id"], image, caption=msg, parse_mode="HTML")
-                    else:
-                        await bot.send_message(tg_info["channel_id"], msg, parse_mode="HTML")
+                if image:
+                    await bot.send_photo(tg_info["channelId"], image, caption=msg, parse_mode="HTML")
+                else:
+                    await bot.send_message(tg_info["channelId"], msg, parse_mode="HTML")
 
         print(time() + "checking finished")
-        if config["log file"]:
+        if config["logging"]:
             with open("exception.log", "a") as logfile:
                 logfile.write(time() + "checking finished\n")
->>>>>>> e5ff7661b3e5b888face2e490afc206a577b2047
 
         await asyncio.sleep(config["sleep"])
 
@@ -111,7 +104,7 @@ if __name__ == "__main__":
     new = None
 
     while True:
-        bot = Bot(tg_info["bot_token"])
+        bot = Bot(tg_info["botToken"])
         aio_loop = asyncio.get_event_loop()
 
         try:
@@ -120,14 +113,14 @@ if __name__ == "__main__":
             pass
         except BaseException as err:
             print(time() + str(err) + ": " + str(new))
-            if config["log file"]:
+            if config["logging"]:
                 with open("exception.log", "a") as log_file:
                     log_file.write(time() + str(err) + ": " + str(new) + "\n")
         finally:
             aio_loop.run_until_complete(bot.close())
             print(time() + "loop finished")
-            if config["log file"]:
+            if config["logging"]:
                 with open("exception.log", "a") as log_file:
                     log_file.write(time() + "loop finished\n")
 
-            sleep(config["retry sleep"])
+            sleep(config["retrySleep"])
